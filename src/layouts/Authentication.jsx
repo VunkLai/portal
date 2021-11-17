d20fab07ce947d634c2ca34dc48268f653189338
Vunk.Lai <vunk.lai@gmail.com>
2021-11-18 00:05:18 +0800

    [feat] Create the default auth layout with nav-bar

--- src/layouts/Authentication.jsx
+++ src/layouts/Authentication.jsx
@@ -1,11 +1,26 @@
+import { styled } from "@mui/material/styles";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+const Root = styled("div")(({ theme }) => ({}));
+
+const Wrapper = styled("div")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+  padding: theme.spacing(3),
+}));
 
 function Authentication() {
   return (
-    <>
-      <Outlet />
-    </>
+    <Root>
+      <NavBar />
+      <Wrapper>
+        <Outlet />
+      </Wrapper>
+    </Root>
   );
 }
 
