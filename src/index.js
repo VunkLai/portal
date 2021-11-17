8be434e3b60ac37300f89d89e84786faa50acbb4
Vunk.Lai <vunk.lai@gmail.com>
2021-11-18 00:05:39 +0800

    [feat] Create the login page with a simple form

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { AuthProvider } from "./contexts/AuthContext";
 import { SettingsProvider } from "./contexts/SettingsContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <SettingsProvider>
-        <App />
-      </SettingsProvider>
-    </BrowserRouter>
+    <SettingsProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </SettingsProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,5 +1,42 @@
+import Button from "@mui/material/Button";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+
 function Login() {
-  return <>Login</>;
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const [form, setForm] = React.useState({ username: "", password: "" });
+
+  React.useEffect(() => {
+    if (auth.isAuthenticated) {
+      navigate("/", { replace: true });
+    }
+  }, []);
+
+  const login = async () => {
+    await auth.login({ ...form });
+    navigate("/", { replace: false });
+  };
+
+  const handleClick = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <>
+      username:{" "}
+      <input value={form.username} name="username" onChange={handleClick} />
+      password:{" "}
+      <input
+        value={form.password}
+        name="password"
+        onChange={handleClick}
+        type="password"
+      />
+      <Button onClick={login}>Login</Button>
+    </>
+  );
 }
 
 export default Login;
